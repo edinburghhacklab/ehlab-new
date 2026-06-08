3f37c88c25a20cc44b12abb5832642e24b060192
Aria <me@aria.rip>
2026-06-08 14:38:34 +0100

    shell.nix: remove outdated message

--- shell.nix
+++ shell.nix
@@ -10,11 +10,4 @@ pkgs.mkShell {
     gawk
     lua
   ];
-
-  shellHook = ''
-    echo "Available commands:"
-    echo "  just dev    - Watch and serve simultaneously"
-    echo "  just clean  - Clear the cache"
-    echo "  just deploy - Deploy to production"
-  '';
 }
